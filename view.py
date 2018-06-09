5c25f954dcef71a0e26f7a487431b3ca6262c299
nikita <shubin.mykyta@gmail.con>
2018-06-09 13:31:04 +0300

    +

--- view.py
+++ view.py
@@ -1,9 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def StartWithHello():
     return '<h1>Hello, World!!!!!</h1>'
 
-@app.route('/user/<username>')
-def SayHelloToSomeone(username):
-    return '<h1>Hello, %s !</h1>' % username
+@app.route('/user/')
+def SayHello():
+    user = { 'nickname': 'Shubas' }
+    return render_template("index.html",
+        title = 'This is',
+        user = user)
